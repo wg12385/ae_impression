69adae8e45b22c6867b07045c7212dd350a04ae6
Will Gerrard <wgerrard@vpn-user-245-146.nomadic.bris.ac.uk>
2019-11-22 10:57:45 +0000

    mbook

--- aE_lib/aE_utils.py
+++ aE_lib/aE_utils.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
 
 		for f, file in enumerate(glob.glob(args['files'])):
 
-			sys.exit(0)
 			print(file)
 			mol = nmrmol(molid=f)
 			mol.read_structure(file, args['type'])
