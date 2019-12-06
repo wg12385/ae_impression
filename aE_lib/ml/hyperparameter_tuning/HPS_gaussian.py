132c89dea9585a726a4accc14562ca03e414be73
Will Gerrard <wgerrard@vpn-user-244-070.nomadic.bris.ac.uk>
2019-12-06 11:14:52 +0000

    mbook

--- aE_lib/ml/hyperparameter_tuning/HPS_gaussian.py
+++ aE_lib/ml/hyperparameter_tuning/HPS_gaussian.py
@@ -49,7 +49,7 @@ def gaussian_search(dataset, args):
 				if 'log' in args['param_logs'][param]:
 					next_point_to_probe[param] = 10**next_point_to_probe[param]
 
-		 score, BEST_SCORE, BEST_PARAMS = generic.HPS_iteration(e, dataset, args, next_point_to_probe=next_point_to_probe,
+		score, BEST_SCORE, BEST_PARAMS = generic.HPS_iteration(e, dataset, args, next_point_to_probe=next_point_to_probe,
 															BEST_SCORE=BEST_SCORE, BEST_PARAMS=BEST_PARAMS)
 
 		optimizer.register(params=next_point_to_probe, target=-score)
