1d585b61f8cdcb88644ed4378e9ec0d23257c810
wg12385 <wg12385@bristol.ac.uk>
2020-01-28 11:21:25 +0000

    test test

--- tests/impression_test/predict_test.py
+++ tests/impression_test/predict_test.py
@@ -3,26 +3,34 @@
 from autoENRICH.top_level import CMD_predict
 from autoENRICH.util.argparse_wizard import get_minimal_args
 import glob
+import os
 
 def get_test_cases():
 
-	test_cases = {}
+	test_cases = []
 
-		for modelflag in ['KRR', 'FCHL', 'NN', 'TFM']:
-			for featureflag in ['CMAT', 'aSLATM', 'FCHL', 'ACSF', 'BCAI']:
-				for searchflag in ['grid', 'gaussian', 'random']:
-					args = get_minimal_args()
-					args['var'] = 2
-					for targetflag in ['HCS', 'CCS', 'NCS', '1JCH', '3JHH']:
-						model = modelflag + '_' + targetflag + '_' + featureflag + '_' + searchflag + '.pkl'
+	for modelflag in ['KRR', 'FCHL']:
+		for featureflag in ['CMAT', 'aSLATM', 'FCHL', 'ACSF']:
+			for searchflag in ['grid', 'gaussian', 'random']:
+				args = get_minimal_args()
+				args['var'] = 2
+				args['models'] = []
+				for targetflag in ['HCS', 'CCS', 'NCS', '1JCH', '3JHH']:
+					model = 'test_models/' + modelflag + '_' + targetflag + '_' + featureflag + '_' + searchflag + '_model.pkl'
+					if os.path.isfile(model):
 						args['models'].append(model)
-					test_cases.append(model)
+					else:
+						continue
+				if 'None' in args['models'] or len(args['models']) == 0:
+					continue
+				else:
+					test_cases.append(args)
 
 	return test_cases
 
 def test_setup():
 	status = 'Pass'
-	case = get_minimal_args
+	case = get_minimal_args()
 	case['Command'] = 'setup_predict'
 	case['test_sets'] = ['test_files/test*.sdf']
 	try:
@@ -41,6 +49,7 @@ def test_predict():
 	cases = get_test_cases()
 
 	for case in cases:
+		print('CASE _____', case['featureflag'], case['modelflag'], case['targetflag'], case['searchflag'])
 		case['Command'] = 'predict'
 		case['test_sets'] = ['test_files/test*.sdf']
 		try:
@@ -54,9 +63,7 @@ def test_predict():
 	return status
 
 
-
-
 if __name__ == "__main__":
 	status1 = test_setup()
-	status2 = test_train()
+	status2 = test_predict()
 	print(status1, status2)
--- tests/impression_test/train_test.py
+++ tests/impression_test/train_test.py
@@ -3,43 +3,46 @@
 # Test setup_train and train commands
 
 from autoENRICH.top_level import CMD_trainmodel
-from autoENRICH.util.argparse_wizard import get_minimal_args
+from autoENRICH.util.argparse_wizard import get_minimal_args, run_wizard
+from autoENRICH.util.flag_handler import hdl_targetflag, flag_combos
+from autoENRICH.util.arguments.argparser import combine_args
 
+import glob
+import os
+import sys
+import traceback
 
 def construct_testcases():
 
 	argsets = []
 
 	for targetflag in ['HCS', 'CCS', 'NCS', '1JCH', '3JHH']:
-		for modelflag in ['KRR', 'FCHL', 'NN', 'TFM']:
+		for modelflag in ['KRR', 'FCHL']: # NN TFM
 			for featureflag in ['CMAT', 'aSLATM', 'FCHL', 'ACSF', 'BCAI']:
 				for searchflag in ['grid', 'gaussian', 'random']:
 					args = get_minimal_args()
 					args['epochs'] = 1
 					args['cv_steps'] = 2
+					args['grid_density'] = 1
+					args['load'] = False
 					args['targetflag'] = targetflag
 					args['modelflag'] = modelflag
 					args['featureflag'] = featureflag
 					args['searchflag'] = searchflag
+					args['output_dir'] = './test_models/'
 					argsets.append(args)
 	return argsets
 
 def test_setup():
 	status = 'Pass'
 	cases = construct_testcases()
-	for testcase in cases:
-
+	for case in cases:
+		case['prefs'] = 'default'
 		case['Command'] = 'setup_train'
 		case['training_set'] = 'test_files/train*.sdf'
 		case['python_env'] = 'IMPgen1'
 
-		try:
-			CMD_trainmodel.setup_trainmodel(case)
-		except Exception as e:
-			print('ERROR IN TEST -------------------------------------------')
-			print(case)
-			print(e)
-			status = 'Fail'
+		CMD_trainmodel.setup_trainmodel(case)
 
 	print('Test status: ', status)
 
@@ -49,19 +52,30 @@ def test_setup():
 def test_train():
 	status = 'Pass'
 	cases = construct_testcases()
-	for testcase in cases:
-
+	for case in cases:
+		print('CASE _____', case['featureflag'], case['modelflag'], case['targetflag'], case['searchflag'])
+		case['prefs'] = 'default'
 		case['Command'] = 'train'
 		case['training_set'] = 'test_files/train*.sdf'
 		case['python_env'] = 'IMPgen1'
+		case['feature_optimisation'] = 'True'
 
-		try:
-			CMD_trainmodel.trainmodel(case)
-		except Exception as e:
-			print('ERROR IN TEST -------------------------------------------')
-			print(case)
-			print(e)
-			status = 'Fail'
+		wiz_args = run_wizard(case, default=True)
+		case = combine_args(case, {}, wiz_args, case)
+		print(case['epochs'])
+		# check target flag is valid (nJxy or XCS):
+		target = hdl_targetflag.flag_to_target(case['targetflag'])
+		# 0 is the bad number
+		if target == 0:
+			print('Invalid target flag, ', case['targetflag'])
+			continue
+
+		# check flag combination for feature / model
+		if not flag_combos.check_combination(case['modelflag'], case['featureflag']):
+			print('Invalid model and feature combination: ', case['modelflag'], case['featureflag'])
+			continue
+
+		CMD_trainmodel.trainmodel(case)
 
 	print('Test status: ', status)
 
@@ -71,5 +85,9 @@ def test_train():
 
 if __name__ == "__main__":
 	status1 = test_setup()
+	submit_files = glob.glob('*.submit')
+	for file in submit_files:
+		os.remove(file)
+
 	status2 = test_train()
 	print(status1, status2)
