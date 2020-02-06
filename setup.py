1d4e7cccf4415570660207cc9ae08d28fe4f0bab
Will Gerrard <wgerrard@eduroam-89-54.nomadic.bris.ac.uk>
2020-02-06 13:11:17 +0000

    mbook, cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='autoENRICH',
-     version='1.9',
+     version='1.91',
      scripts=['bin/autoenrich', 'bin/impression', 'bin/ae_utils'] ,
      author="Will Gerrard",
      author_email="will.gerrard@bristol.ac.uk",
