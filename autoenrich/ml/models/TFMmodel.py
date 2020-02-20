baf9a2b0f204a47de43916b23ff8183a9653b6f2
Will Gerrard <wgerrard@Wills-MacBook-Pro.local>
2020-02-20 14:07:03 +0000

    added tqdm timer to epochs

--- autoenrich/ml/models/TFMmodel.py
+++ autoenrich/ml/models/TFMmodel.py
@@ -32,8 +32,9 @@ import autoenrich.ml.models.BCAI.predictor as BCAI_predict
 from .BCAI.modules import radam
 
 import sys
-
+import time
 import random
+import tqdm
 
 class TFMmodel(genericmodel):
 
@@ -89,7 +90,7 @@ class TFMmodel(genericmodel):
 
 		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 		if torch.cuda.device_count() > 1:
-			print("Using", torch.cuda.device_count(), "GPUs!")
+			print("Using", torch.cuda.device_count(), "GPUs")
 			self.model = torch.nn.DataParallel(model)
 
 		self.model = BCAI_graph.GraphTransformer(dim=int(self.params['d_model']/int(self.params['n_head']))*int(self.params['n_head']), n_layers=int(self.params['n_layer']), d_inner=int(self.params['d_inner']),
@@ -116,9 +117,18 @@ class TFMmodel(genericmodel):
 
 		para_model = self.model.to(device)
 
-		for tr_epoch in range(int(self.params['tr_epochs'])):
-			#print('\ttrepoch: ', tr_epoch, '/', train_epochs)
+
+		st_time = time.time()
+		loss1, loss2, loss2 = BCAI_train.epoch(train_loader, self.model, optimizer, self.params['learning_rate'])
+		nd_time = time.time()
+		duration = nd_time - st_time
+		string = "Model training started at {0:<5s}, expected to take {1:<5d}Hrs, will finish at {2:<5s}".format(time.ctime(st_time),
+																						duration*tr_epochs/(60*60),
+																						time.ctime(st_time+duration*tr_epochs)
+		print(string)
+		for tr_epoch in tqdm(range(1, int(self.params['tr_epochs']))):
 			loss1, loss2, loss2 = BCAI_train.epoch(train_loader, self.model, optimizer, self.params['learning_rate'])
+
 		self.trained = True
 
 	def predict(self, test_x, train_x=[]):
@@ -128,7 +138,10 @@ class TFMmodel(genericmodel):
 
 		#BCAI_predict.single_model_predict(test_loader, self.model, 'test')
 		MAX_BOND_COUNT = 500
-		dev = "cuda"
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		if torch.cuda.device_count() > 1:
+			print("Using", torch.cuda.device_count(), "GPUs!")
+			self.model = torch.nn.DataParallel(model)
 		self.model.to(dev)
 		self.model.eval()
 		y_predictions = []
